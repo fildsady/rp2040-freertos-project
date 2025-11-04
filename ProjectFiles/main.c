27a969e1d8b7d70baf6c6cbc03e0a342ceb4bee8
Anon <53999729+fildsady@users.noreply.github.com>
2025-11-05 02:55:13 +0700

    v3

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "hardware/adc.h"
+
 
 #define LED_PIN 0
 
@@ -14,9 +16,20 @@ void led_task()
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        vTaskDelay(250);
+        vTaskDelay(500);
         gpio_put(LED_PIN, 0);
-        vTaskDelay(250);
+        vTaskDelay(5001);
+    }
+}
+
+
+void adc_read_task()
+{
+    while (true){
+
+        printf("Hello World\r\n");
+        vTaskDelay(500);
+
     }
 }
 
@@ -24,7 +37,10 @@ int main()
 {
     stdio_init_all();
 
+    adc_init();
+
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(adc_read_task, "LED_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1){};
