aaa99be7c8e71958f9f83f49adc25b875e7ba911
Anon <53999729+fildsady@users.noreply.github.com>
2025-11-05 02:08:36 +0700

    g

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -6,14 +6,14 @@
 
 void led_task()
 {   
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    int LED_PIN = 0;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        vTaskDelay(100);
+        vTaskDelay(500);
         gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
+        vTaskDelay(500);
     }
 }
 
