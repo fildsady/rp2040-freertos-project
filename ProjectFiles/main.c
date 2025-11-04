beeb0fbbe9ec3c13cf8e01826082348a145d67d0
Anon <53999729+fildsady@users.noreply.github.com>
2025-11-05 02:15:36 +0700

    v2

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -4,16 +4,19 @@
 #include "pico/stdlib.h"
 
 
+#define LED_PIN 0
+
+
 void led_task()
 {   
-    int LED_PIN = 0;
+
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        vTaskDelay(500);
+        vTaskDelay(250);
         gpio_put(LED_PIN, 0);
-        vTaskDelay(500);
+        vTaskDelay(250);
     }
 }
 
